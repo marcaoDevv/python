976db7481f3a531404ea1cea9d066e7bb2756a77
marcaoDevv <m.fabio47@gmail.com>
2023-01-19 21:25:30 -0300

    metodo_format.py

---
+++ metodo_format.py
@@ -0,0 +1,13 @@
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+
+print('O faturamento da loja foi ' + str(faturamento) + '. O custo da loja foi ' + str(custo) + '. Assim, o lucro da loja foi de ' + str(lucro))
+
+#Método format
+print('O faturamento da loja foi {}. O custo da loja foi {}. Assim, o lucro da loja foi de {}' .format(faturamento, custo, lucro))
+
+nome = 'Marcos'
+idade = 22
+
+print('Meu nome é: {}. Minha idade é: {} anos' .format(nome, idade))
